e7f0bc346a496d3ac9ef303cf21f68a9d13d16c0
sjonesjones917 <sjones.jones917@gmail.com>
2026-08-25 12:08:16 -0500

    Fix generated unresolved validator syntax

--- .practical100-pr2-fix.mjs
+++ .practical100-pr2-fix.mjs
@@ -5,4 +5,5 @@ for(const name of ['path','key']){
   const re=new RegExp('\\\\*\\$\\{'+name+'\\}','g');
   s=s.replace(re,'\\${'+name+'}');
 }
+s=s.replace("issues.push(issue('INVALID_UNRESOLVED_KIND',\\`${path}/kind\\`,'Unresolved kind is not controlled.');","issues.push(issue('INVALID_UNRESOLVED_KIND',\\`${path}/kind\\`,'Unresolved kind is not controlled.'));");
 fs.writeFileSync(p,s);
